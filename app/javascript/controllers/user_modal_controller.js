d454b531d789fee6a1286e7665b35f4ac45d7779
sid9568 <sid20319@gmail.com>
2023-05-25 12:19:38 +0530

    first commit

---
+++ app/javascript/controllers/user_modal_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="user-modal"
+export default class extends Controller {
+  connect() {
+    console.log("conntected")
+  }
+  initialize(){
+    this.element.setAttribute("data-action", "click->user-modal#showModal")
+  }
+  showModal(event){
+    event.preventDefault()
+    this.url = this.element.getAttribute("href")
+    fetch(this.url, {
+      headers: {
+        Accept: "text/vnd.turbo-stream.html"
+      }
+    })
+    .then(response => response.text())
+    .then(html => Turbo.renderStreamMessage(html))
+  }
+}
